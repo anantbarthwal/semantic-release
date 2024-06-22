c83166ac6a5c2e6752cce686819b1fda5228d973
anant <anant.barthwal@gmail.com>
2024-06-22 22:58:53 +0530

    FEAT: initial commit

---
+++ src/main/java/com/anantcodeprojects/semantic_release/SemanticReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.anantcodeprojects.semantic_release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anantcodeprojects/semantic_release/SemanticReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anantcodeprojects.semantic_release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
